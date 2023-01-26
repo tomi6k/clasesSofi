a984de95a9a3c5b55c1f3e77a1ce175c3fac17b8
Tomás Canavidez <tms1csgo@gmail.com>
2023-01-26 20:18:33 -0300

    subo clase 2

---
+++ Clase 2/clase.py
@@ -0,0 +1,9 @@
+# TIPOS DE DATOS
+ 
+string = "Hola mundo"  # Cadena de texto o palabras o un texto sin mas
+ 
+entero = 23 # Numeros enteros sin parte decimal
+
+flotante = 3.12 # Numeros con parte decimal
+ 
+booleano = False # Aquellos datos que indican una veracidad Verdadero o falso 

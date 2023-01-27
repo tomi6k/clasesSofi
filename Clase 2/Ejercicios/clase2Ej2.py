8bd5194223df00253a523f3d2c11ad8ae8ce7757
sqlote <tms1csgo@gmail.com>
2023-01-27 19:03:19 -0300

    resolucion del segundo ejercicio, segunda clase

--- Clase 2/Ejercicios/clase2Ej2.py
+++ Clase 2/Ejercicios/clase2Ej2.py
@@ -1,1 +1,12 @@
 #Crear un programa que solicite el nombre al usuario, lo salude y le pida 2 numeros para dividir.
+nombre = int(imput("ingrese su nombre"))
+
+print("hola" , nombre )
+
+numero1 = int(imput("ingresa un numero"))
+
+numero2 = int(imput("ingresa otro numero"))
+
+operacion = numero1 / numero2
+
+print("el resultado de la divicion es" , operacion)

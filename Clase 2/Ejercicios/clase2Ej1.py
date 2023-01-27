96b73e262604eeb8b20cbae09bb7f9cac0afccbc
sqlote <tms1csgo@gmail.com>
2023-01-27 18:58:02 -0300

    resolucion del primer ejercicio de la segunda clase

--- Clase 2/Ejercicios/clase2Ej1.py
+++ Clase 2/Ejercicios/clase2Ej1.py
@@ -1,1 +1,8 @@
 # Crear un programa que el usuario ingrese 2 numeros y se sumen.
+numero1 = int(input("ingrese un numero"))
+
+numero2 = int(input("ingrese otro numero"))
+
+operacion = numero1 + numero2
+
+print("el resultado de la suma es" , operacion)

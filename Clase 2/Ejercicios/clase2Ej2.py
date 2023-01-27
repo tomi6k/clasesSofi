dad81847b9d1abd7b2006b65a9069fb261d08262
Tomás Canavidez <tms1csgo@gmail.com>
2023-01-27 20:55:12 -0300

    corrijo ej2 clase 2

--- Clase 2/Ejercicios/clase2Ej2.py
+++ Clase 2/Ejercicios/clase2Ej2.py
@@ -1,11 +1,11 @@
 #Crear un programa que solicite el nombre al usuario, lo salude y le pida 2 numeros para dividir.
-nombre = int(imput("ingrese su nombre"))
+nombre = str(input("ingrese su nombre "))
 
 print("hola" , nombre )
 
-numero1 = int(imput("ingresa un numero"))
+numero1 = int(input("ingresa un numero "))
 
-numero2 = int(imput("ingresa otro numero"))
+numero2 = int(input("ingresa otro numero "))
 
 operacion = numero1 / numero2
 

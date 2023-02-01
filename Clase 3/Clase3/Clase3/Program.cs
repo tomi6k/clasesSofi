20363decc4eb29b0691221043d65c9a612c8cf73
Tomás Canavidez <tms1csgo@gmail.com>
2023-01-31 22:40:15 -0300

    subo clase 3

---
+++ Clase 3/Clase3/Clase3/Program.cs
@@ -0,0 +1,35 @@
+﻿// El usuario ingresará su nombre y sexo y el sistema le dira si es guei o no
+
+//Console.WriteLine("Ingrese su nombre: ");
+//string nombre = Console.ReadLine();
+
+//Console.WriteLine("Hola, " + nombre + "¿que genero sos?");
+
+//string genero = Console.ReadLine();
+
+//if (genero == "Hombre")
+//{
+//    Console.WriteLine("Usted es un hombre");
+//} else if (genero == "Mujer") {
+//    Console.WriteLine("Usted es una mujer");
+//} else
+//{
+//    Console.WriteLine("Usted es otro genero");
+//}
+
+
+//Console.ReadKey();
+
+
+int contador = 0;
+
+while (contador <= 50)
+{
+    contador++;
+    Console.WriteLine(contador);
+    if (contador == 45)
+    {
+        Console.WriteLine("Se dieron 45 vueltas");
+        break;
+    }
+}
